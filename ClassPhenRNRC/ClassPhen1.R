5eff7a368aff03e1f8da828bf0027ea7a59af232
3khoz <30305484+3khoz@users.noreply.github.com>
2017-09-01 08:50:37 -0300

    nada importante

--- ClassPhenRNRC/ClassPhen1.R
+++ ClassPhenRNRC/ClassPhen1.R
@@ -3,9 +3,18 @@ library(raster)
 library(ncdf4)
 library(maptools)
 
+#########  DEFINIR ESPACIO DE TRABAJO
+mypath="Z:/ProyectoClassPhen/L8_RNRC/" # selecciona el directorio de los rasters
+folders <- list.files(path=mypath, pattern=glob2rx("LC0823*"), full.names=T)
+
 i=1
-path="C:/Users/idiaz/Downloads/" # selecciona el directorio de los rasters
-lst <- list.files(path=path,pattern='.tar.gz$',full.names=TRUE)  #genera una lista de nombre de todos los archivos
+
+#for(i in 1:length(folders)){
+  mydata<-folders[i]
+  b5<-raster(list.files(path=myimg,pattern=glob2rx("*.nc"),full.names=TRUE))
+  b7<-raster(list.files(path=myimg,pattern=glob2rx("*."),full.names=TRUE))
+  NBR<-((b5-b7)/(b5+b7))
+
 
 h<-substr(lst,26,114)  # extraer solo seccion de fecha
 
@@ -16,3 +25,16 @@ untar(lst[1],files=paste(h[1],"/",x)
 
 evi <- stack(x,varname="sr_evi")
 pixq <- stack(lst,varname="pixel_qa")
+
+
+mypath <- "C:/Users/idiaz/Desktop/Landsat8_Maule/"
+folders <- list.files(path=mypath, pattern=glob2rx("LC08_L1TP_*"), full.names=T)
+
+## Calcular NBR para todas las imagenes
+
+for(i in 1:length(folders)){
+  myimg<-folders[i]
+  date<-substr(myimg,56,63)
+  b5<-raster(list.files(path=myimg,pattern=glob2rx("*_01_T1_B5.TIF"),full.names=TRUE))
+  b7<-raster(list.files(path=myimg,pattern=glob2rx("*_01_T1_B7.TIF"),full.names=TRUE))
+  NBR<-((b5-b7)/(b5+b7))
